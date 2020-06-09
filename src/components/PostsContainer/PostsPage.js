e62e17e11ee0087d5948c430932cb65f7771a24e
numbers0580 <koreankid18@yahoo.com>
2020-06-09 16:07:04 -0600

    Crawled my way through the mud and jungle known as PostContainer and CommentSection, so far

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,21 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, {useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
+import dummyData from "../../dummy-data";
 
 const PostsPage = () => {
   // set up state for your data
+  const [theItem, updateItem] = useState(dummyData);
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+      {/* map through data here to return a Post and pass data as props to Post */
+        theItem.map(function(theData, dataIndex) {
+          return <Post key={dataIndex} post={theData}/>;
+        })
+      }
     </div>
   );
 };
